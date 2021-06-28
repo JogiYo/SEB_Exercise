bd00ad9a6646eefd35fbd37a2c196ff661e0b043
JogiYo <jo930921@gmail.com>
2021-06-28 14:42:40 +0900

    merge arrays in 3 ways 1. using concat 2. using ... seperate operator 3. using push and ... seperate operator

---
+++ Array/mergeArrays.js
@@ -0,0 +1,19 @@
+function mergeArrays_concat(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+
+function mergeArrays_seperate_operator(arr1, arr2) {
+    return [...arr1, ...arr2];
+}
+
+function mergeArrays_SO_and_push(arr1, arr2) {
+    arr1.push(...arr2);
+    return arr1;
+}
+
+str_arr1 = ['star', 'apple', 'banana'];
+str_arr2 = ['monkey', 'code'];
+
+console.log(mergeArrays_concat(str_arr1, str_arr2));
+console.log(mergeArrays_seperate_operator(str_arr1, str_arr2));
+console.log(mergeArrays_SO_and_push(str_arr1, str_arr2));
