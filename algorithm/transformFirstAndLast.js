fb0501749029f8dbbfe25baba7552863bd432eb2
JogiYo <jo930921@gmail.com>
2021-07-05 08:45:00 +0900

    transformFirstAndLast algorithm exercise 배열을 입력받아 차례대로 배열의 첫 요소와 마지막 요소를 키와 값으로 하는 객체를 리턴

---
+++ algorithm/transformFirstAndLast.js
@@ -0,0 +1,10 @@
+// 배열을 입력받아 차례대로 배열의 첫 요소와 마지막 요소를 키와 값으로 하는 객체를 리턴
+function transformFirstAndLast(arr) {
+    let obj = {};
+    if(arr.length === 0) {
+        return obj;
+    }
+
+    obj[arr[0]] = arr[arr.length-1];
+    return obj;
+}
