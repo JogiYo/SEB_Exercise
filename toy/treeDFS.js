f82270a01ca868a0ccf1f9ec95bd2033cfb2e318
JogiYo <jo930921@gmail.com>
2021-07-26 09:28:13 +0900

    treeDFS.js code modified visit 변수가 필요하지 않을 것 같아 삭제

--- toy/treeDFS.js
+++ toy/treeDFS.js
@@ -4,13 +4,9 @@ let dfs = function (node) {
 
     let search = (node) => {
         result.push(node.value)
-        let visit = new Array(node.children.length).fill(false);
-
-        for(let i = 0; i < visit.length; ++i) {
-            if(!visit[i]) {
-                visit[i] = true;
-                search(node.children[i]);
-            }
+    
+        for(let i = 0; i < node.children.length; ++i) {
+            search(node.children[i]);
         }
     }
 
@@ -30,3 +26,16 @@ Node.prototype.addChild = function (child) {
     this.children.push(child);
     return child;
 };
+
+let root = new Node(1);
+let rootChild1 = root.addChild(new Node(2));
+let rootChild2 = root.addChild(new Node(3));
+let leaf1 = rootChild1.addChild(new Node(4));
+let leaf2 = rootChild1.addChild(new Node(5));
+let output = dfs(root);
+console.log(output); // --> [1, 2, 4, 5, 3]
+
+leaf1.addChild(new Node(6));
+rootChild2.addChild(new Node(7));
+output = dfs(root);
+console.log(output); 
