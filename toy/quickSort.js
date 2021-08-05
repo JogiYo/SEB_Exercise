7a3c5018375c751d3710628540e324c7c895b8ed
JogiYo <jo930921@gmail.com>
2021-08-05 09:56:45 +0900

    quickSort.js toy complete 퀵 정렬 알고리즘을 이용한다. 레퍼런스는 처음보는 풀이였기 때문에 참고를 하자. 기존 알고리즘으로 작성했을 때는 시간초과가 계속 나왔다.

--- toy/quickSort.js
+++ toy/quickSort.js
@@ -1,37 +1,56 @@
-// https://blockdmask.tistory.com/177
 const quickSort = function (arr, standard = (el) => el) {
-    const q_sort =  (arr, left, right) => {
+    const partition = function(arr, left, right) {
         let pivot = standard(arr[left]);
         let start = left + 1;
         let end = right;
 
-        while(start <= end) {
-            while(standard(arr[start]) < pivot) start++;
+        while(start < end) {
             while(standard(arr[end]) > pivot) end--;
-
-            if(start <= end) {
-                let temp = arr[start];
-                arr[start] = arr[end];
-                arr[end] = temp;
-            }
-        }
-
-        if(left < right) {
-            let temp = arr[left];
-            arr[left] = arr[end];
+            while(start < end && standard(arr[start]) < pivot) start++;
+            
+            let temp = arr[start];
+            arr[start] = arr[end];
             arr[end] = temp;
-
-            q_sort(arr, left, end - 1);
-            q_sort(arr, end + 1, right);
         }
+        arr[left] = arr[end];
+        arr[end] = pivot;
 
-        return;
+        return end;
     }
 
-    let left = standard(0);
-    let right = standard(arr.length-1);
-    return q_sort(arr, left, right);
+    const q_sort =  (arr, left, right) => {
+        if(left >= right) return;
+
+        let i = partition(arr, left, right);
+        q_sort(arr, left, i-1);
+        q_sort(arr, i+1, right);
+    }
+
+    let left = 0
+    let right = arr.length-1;
+    q_sort(arr, left, right);
+    return arr;
 };
 
-let output = quickSort([4, 5, 1, 2, 11, 8, 3, 1, 2, 5 ]);
-console.log(output); // --> [1, 3, 21]
+// reference code
+function quickSort_ref(arr, transform = (item) => item) {
+    if (arr.length <= 1) return arr;
+  
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (transform(arr[i]) < transform(pivot)) left.push(arr[i]);
+      else right.push(arr[i]);
+    }
+  
+    const lSorted = quickSort_ref(left, transform);
+    const rSorted = quickSort_ref(right, transform);
+    return [...lSorted, pivot, ...rSorted];
+}
+
+let output = quickSort([4, 5, 1, 2, 10, 8, 3, 7, 6, 9]);
+console.log(output);
+output = quickSort_ref([4, 5, 1, 2, 10, 8, 3, 7, 6, 9]);
+console.log(output);
