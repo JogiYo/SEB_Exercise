a3b295e408250bd78b585619319ff4337f35bfa3
JogiYo <jo930921@gmail.com>
2021-08-11 09:44:25 +0900

    -ing

--- toy/getItemFromTwoSortedArrays.js
+++ toy/getItemFromTwoSortedArrays.js
@@ -1,25 +1,38 @@
 const getItemFromTwoSortedArrays = function (arr1, arr2, k) {
-    let merged_arr = [];
+    while(k > 1) {
+        let left_idx = 0;
+        let right_idx = 0;
 
-    while(arr1.length !== 0 || arr2.length !== 0)  {
-        if(arr1.length !== 0 && arr2.length !== 0) {
-            if(arr1[0] < arr2[0]) {
-                merged_arr.push(arr1.shift());
-            }
-            else {
-                merged_arr.push(arr2.shift());
-            }
+        let count = Math.floor(k/2);
+        if(count < arr1.length) left_idx = count - 1;
+        if(count < arr2.length) right_idx = count - 1;
+
+        if(arr1.length === 0) {
+            return arr2[k-1];
+        }
+        if(arr2.length === 0) {
+            return arr1[k-1];
         }
 
-        else if(arr1.length !== 0) {
-            merged_arr.push(arr1.shift());
+        if(arr1[left_idx] < arr2[right_idx]) {
+            if(count > arr1.length) {
+                k -= arr1.length; 
+            }
+            else k -= count;
+
+            arr1 = arr1.slice(left_idx + 1);
         }
-        else if(arr2.length !== 0) {
-            merged_arr.push(arr2.shift());
+        else {
+            if(count > arr2.length) {
+                k -= arr2.length; 
+            }
+            else k -= count;
+            
+            arr2 = arr2.slice(right_idx + 1);
         }
     }
 
-    return merged_arr[k-1];
+    return Math.min(arr1[0], arr2[0]);
 };
 
 let arr1 = [1, 4, 8, 10];
@@ -31,3 +44,41 @@ arr1 = [1, 1, 2, 10];
 arr2 = [3, 3];
 result = getItemFromTwoSortedArrays(arr1, arr2, 4);
 console.log(result); // --> 3
+
+// const getItemFromTwoSortedArrays = function (arr1, arr2, k) {
+//     let left_idx = 0;
+//     let right_idx = 0;
+
+//     while(k > 0) {
+//         let count = Math.ceil(k/2);
+//         let left_count = count;
+//         let right_count = count;
+
+//         if(left_idx === arr1.length) {
+//             right_idx += k;
+//             break;
+//         }
+
+//         if(right_idx === arr2.length) {
+//             left_idx += k;
+//             break;
+//         }
+
+//         if(left_count > arr1.length - left_idx) left_count = arr1.length - left_idx;
+//         if(right_count > arr2.length - right_idx)  right_count = arr2.length - right_idx;
+
+//         if(arr1[left_idx + (left_count - 1)] < arr2[right_idx + (right_count - 1)]) {
+//             left_idx += left_count;
+//             k -= left_count;
+//         }
+//         else {
+//             right_idx += right_count;
+//             k -= right_count;
+//         }
+//     }
+
+//     let left = arr1[left_idx - 1] || -1;
+//     let right = arr2[right_idx - 1] || -1;
+
+//     return Math.max(left, right);
+// };
