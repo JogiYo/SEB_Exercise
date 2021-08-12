63fb94c65d9dffeff6ccbafd95598d5806be8a79
JogiYo <jo930921@gmail.com>
2021-08-12 09:50:07 +0900

    LPS.js toy complete LPS: 주어진 문자열의 가장 긴 접두어이자 접미어(Longest Prefix which is also Suffix) non-overlapping: 접두어와 접미어는 서로 겹치는 부분이 없어야 합니다. 다시 말해, prefix와 suffix는 문자열의 동일한 인덱스에 위치한 문자를 요소로 가지면 안 됩니다.
    prefix(접두어)는 문자열의 첫 인덱스부터 시작하는 모든 부분 문자열을 의미합니다.
    suffix(접미어)는 문자열의 마지막 인덱스부터 시작하는 모든 부분 문자열을 의미합니다.

---
+++ toy/LPS.js
@@ -0,0 +1,33 @@
+const LPS = function (str) {
+    // TODO: 여기에 코드를 작성합니다.
+    let resultStr = '';
+    
+    for (let i = 1; i <= str.length / 2; ++i) {
+      let prefix = str.slice(0, i);
+      let suffix = str.slice(str.length - i);
+      
+      if (prefix === suffix) {
+        resultStr = prefix;
+      }
+    };
+    
+    return resultStr.length;
+  
+    // const result = str.match(/^(\w*).*\1$/);
+    // return result[1].length;
+};
+
+let output = LPS('abbbcc');
+console.log(output); // --> 0
+
+output = LPS('aaaa');
+console.log(output); // --> 2
+// prefix: str.slice(0, 2)
+// suffix: str.slice(2)
+// non-overlapping 조건이 없는 경우 정답은 4 입니다.
+
+output = LPS('aaaaa');
+console.log(output); // --> 2
+// prefix: str.slice(0, 2)
+// suffix: str.slice(3)
+// non-overlapping 조건이 없는 경우 정답은 5 입니다.
